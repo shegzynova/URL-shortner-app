8ea300e6e598839bab40b94034f117a6fd931b0d
shegzynova <shegzyalabs@gmail.com>
2021-10-03 11:57:52 +0100

    Encode Url function created

--- app/Http/Controllers/ShortUrlController.php
+++ app/Http/Controllers/ShortUrlController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EncodeUrl;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ShortUrlController extends Controller
 {
-    //
+    
+
+    public function encode(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'url' => 'required|url|unique:encode_urls,original_url',
+        ]);
+
+
+        $errors = $validator->errors();
+        if ($validator->fails() && ($errors->first() != "")) {
+            return response()->json($errors);
+            // return back()->withInput();
+        }
+            $encode = EncodeUrl::create([
+                'original_url' => $request->url,
+            ]);
+
+            if($encode){
+                $encode->update([
+                    'short_url' => $this->generateRandomString()
+                ]);
+            }
+
+            return response()->json([
+                'url' => $encode->short_url,
+            ]);
+
+    }
+
+
 }
