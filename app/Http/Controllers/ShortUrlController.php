63ea53fd1ef662513a14053393180bac7f443b57
shegzynova <shegzyalabs@gmail.com>
2021-10-03 12:04:28 +0100

    Random String Function Added

--- app/Http/Controllers/ShortUrlController.php
+++ app/Http/Controllers/ShortUrlController.php
@@ -40,4 +40,15 @@ class ShortUrlController extends Controller
     }
 
 
+
+    function generateRandomString($length = 6) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
 }
